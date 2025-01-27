a344e90208a499ad25d074d0336af407b4a7c28e
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2025-01-27 14:10:18 +0530

    bootstrap
    -- installing dependencies for AWS bootstraping the cluster

---
+++ shell.sh
@@ -0,0 +1,6 @@
+# PySpark and Dependencies
+sudo python3 -m pip install --upgrade pip
+sudo python3 -m pip install boto3 pandas opencv-python tqdm matplotlib
+
+# Deep Learning Frameworks
+sudo python3 -m pip install torch torchvision
